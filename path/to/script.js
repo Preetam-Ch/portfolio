f8e15305937b291ef9663e09d2a373d1dd2d576b
Preetam <preetam.ch1@gmail.com>
2023-05-04 12:40:56 +0530

    this

---
+++ path/to/script.js
@@ -0,0 +1,7 @@
+function showPopup(popupId) {
+    document.getElementById(popupId).style.display = "block";
+}
+
+function hidePopup(popupId) {
+    document.getElementById(popupId).style.display = "none";
+}
