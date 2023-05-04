ba3d25cc7d1f9a9edb7ba6a0b5c7325923cfd2da
Preetam <preetam.ch1@gmail.com>
2023-05-04 12:42:36 +0530

    final

--- js/style-switcher.js
+++ js/style-switcher.js
@@ -7,9 +7,9 @@
     };
     var opts = $.extend({}, defualts, options);
     var tagEle =
-      "querySelectorAll" in document
-        ? document.querySelectorAll("." + opts.tag)
-        : getClass(opts.tag),
+        "querySelectorAll" in document
+          ? document.querySelectorAll("." + opts.tag)
+          : getClass(opts.tag),
       paper = $(this)[0];
     (RADIUS = opts.ballSize),
       (fallLength = 300),
@@ -150,45 +150,51 @@ $(document).ready(function () {
 
 
 
-
+  
 
 
 
 /* ===============toggle style switcher ============== */
 const styleSwitcherToggle = document.querySelector(".style-switcher-toggler");
-styleSwitcherToggle.addEventListener("click", () => {
-  document.querySelector(".style-switcher").classList.toggle("open");
+styleSwitcherToggle.addEventListener("click",() => {
+    document.querySelector(".style-switcher").classList.toggle("open");
 })
 // hide syle-switcher icon on scroll//
-window.addEventListener("scroll", () => {
-  if (document.querySelector(".style-switcher").classList.contains("open")) {
-    document.querySelector(".style-switcher").classList.remove("open");
-  }
+window.addEventListener("scroll",()=>{
+    if(document.querySelector(".style-switcher").classList.contains("open"))
+      {
+        document.querySelector(".style-switcher").classList.remove("open");
+      }
 })
 /*===============theme colors ============== */
 const alternateStyles = document.querySelectorAll(".alternate-style");
-function setActiveStyle(color) {
-  alternateStyles.forEach((style) => {
-    if (color === style.getAttribute("title")) {
-      style.removeAttribute("disabled");
-    }
-    else {
-      style.setAttribute("disabled", "true");
-    }
-  });
+function setActiveStyle(color)
+{
+alternateStyles.forEach((style) => {
+if (color === style.getAttribute("title"))
+{
+style.removeAttribute("disabled");
+}
+else
+{
+style.setAttribute("disabled","true");
+}
+});
 }
 /*=============== theme light and dark mode ============== */
 const dayNight = document.querySelector(".day-night");
-dayNight.addEventListener("click", () => {
-  dayNight.querySelector("i").classList.toggle("fa-sun");
-  dayNight.querySelector("i").classList.toggle("fa-moon");
-  document.body.classList.toggle("dark");
+dayNight.addEventListener("click",() => {
+    dayNight.querySelector("i").classList.toggle("fa-sun");
+    dayNight.querySelector("i").classList.toggle("fa-moon");
+    document.body.classList.toggle("dark");  
 })
-window.addEventListener("load", () => {
-  if (document.body.classList.contains("dark")) {
-    dayNight.querySelector("i").classList.add("fa-sun");
-  }
-  else {
-    dayNight.querySelector("i").classList.add("fa-moon");
-  }
+window.addEventListener("load",() =>{
+    if(document.body.classList.contains("dark"))
+    {
+        dayNight.querySelector("i").classList.add("fa-sun");
+    }
+    else
+    {
+        dayNight.querySelector("i").classList.add("fa-moon");
+    }
 })
